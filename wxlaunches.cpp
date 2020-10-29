8355d36025ab8950675bacb0f51b513006bb0784
sunburntcat <32531379+sunburntcat@users.noreply.github.com>
2020-10-29 07:45:59 -0500

    Update wxlaunches.cpp

--- wxlaunches.cpp
+++ wxlaunches.cpp
@@ -4,112 +4,149 @@
 using namespace std;
 using namespace eosio;
 
-ACTION wxlaunches::websetlaunch(uint64_t unix_time, name miner, string memo, string wxcondition) {
+ACTION wxlaunches::websetlaunch(name launch_id, 
+                                uint64_t unix_time, 
+                                name miner,
+                                string device_type,
+                                string wxcondition) {
+
   // Only web app can run this Action
   require_auth("weatherapppp"_n);
 
-  uint64_t nextlaunch;
-
-  launches_table_t _launches(get_self(), get_self().value);  
-
-  if (_launches.cbegin() == _launches.cend())
-  {
-    nextlaunch = 1;
-  } else {
-    auto last_itr = _launches.cend(); // Takes the end of the vector
-    last_itr--;
-    nextlaunch = last_itr->launch_number + 1;
-  }
+  launches_table_t _launches(get_self(), get_self().value);
 
   // Add the currently stored variables to the launches table
   _launches.emplace(get_self(), [&](auto& launch) {
-      launch.launch_number = nextlaunch;
+
+      launch.launch_id = launch_id;
       launch.unix_time = unix_time; // Comes as function input
       launch.miner = miner;
-      launch.miner_memo = memo;
+      launch.device_type = device_type;
       launch.wxcondition = wxcondition;
-      launch.reward_sent = false;
+      launch.level_reached = 1000;
+      launch.wx6hrcondition = "N/A";
+
     });
 
 }
 
-ACTION wxlaunches::iotsetlaunch(float lat, float lon, float pressure, uint32_t reward_amount) {
+ACTION wxlaunches::setstation(string owner, 
+                              float latitude, 
+                              float longitude, 
+                              float elevation, 
+                              float reward_increment) {
+
+
   // Only web app can run this Action
-  require_auth("noderedtelos"_n);
-
-  launches_table_t _launches(get_self(), get_self().value);
-  //auto last_itr = _launches.upper_bound(numeric_limits<uint64_t>::max());
-  
-  // Takes last element in container
-  auto last_itr = _launches.cend(); 
-  last_itr--;
-
-  // Add the currently stored variables to the launches table
-  _launches.modify(last_itr, get_self(), [&](auto& launch) {
-      launch.latitude = lat;
-      launch.longitude = lon;
-      launch.surf_pressure_hpa = pressure;
-      launch.reward_amount = reward_amount;
-    });
-
-}
-
-ACTION wxlaunches::addobs(uint64_t unix_time, 
-                          float pressure_hpa, 
-                          float temperature_c, 
-                          float relHumidity, 
-                          float dewPoint_c,
-                          float wind_mph,
-                          float wind_degrees) {
-  
-  // Only self can run this action
   require_auth(get_self());
 
-  // Get our two tables
+  station_table_t _station(get_self(), get_self().value);
+
+  // Get first field
+  auto station_itr = _station.cbegin();
+
+  // Add the currently stored variables to the launches table
+  if ( station_itr == _station.cend()) { // if table is empty
+
+    // Add the station info
+    _station.emplace(get_self(), [&](auto& station) {
+
+      station.id = get_self();
+      station.owner = owner;
+      station.latitude = latitude;
+      station.longitude = longitude;
+      station.elevation = elevation;
+      station.reward_increment = reward_increment;
+
+    });
+  } else {
+
+    // Update station info
+    _station.modify(station_itr, get_self(), [&](auto& station) {
+
+      station.id = get_self();
+      station.owner = owner;
+      station.latitude = latitude;
+      station.longitude = longitude;
+      station.elevation = elevation;
+      station.reward_increment = reward_increment;
+    });
+  }
+}
+
+ACTION wxlaunches::addobs(name launch_id,
+                  uint64_t unix_time, 
+                  float pressure_hpa, 
+                  float temperature_c, 
+                  float dewPoint_c,
+                  float elevation_m) {
+  
+  // Only self can run this action
+  require_auth("noderedtelos"_n);
+
+  // Get our own two tables
   observations_table_t _observations(get_self(), get_self().value);
   launches_table_t _launches(get_self(), get_self().value);
+  station_table_t _station(get_self(), get_self().value);
 
-  // Takes last element in container
-  auto last_itr = _launches.cend();
-  last_itr--;
+  auto launch_itr = _launches.find(launch_id.value);
 
-  name miner = last_itr->miner;
-  bool if_reward_sent = last_itr->reward_sent;
-  uint8_t launch_number = last_itr->launch_number;
-  uint32_t reward_amount = last_itr->reward_amount*10000; // 10,000 = 1 TLOS
-  string miner_memo = last_itr->miner_memo;
+  auto station_itr = _station.find(get_self().value);
+  //auto station_itr = _station.find("wxstationdat"_n.value);
 
-  if (pressure_hpa < 900.0 && !if_reward_sent) {
+  uint32_t reward_amt = (uint32_t)(10000 * station_itr->reward_increment); // 10,000 = 1 TLOS
+  uint16_t last_level = launch_itr->level_reached;
+  name miner = launch_itr->miner;
+
+  uint8_t pressure_hpa_int = (uint8_t)pressure_hpa;
+
+  if( last_level - pressure_hpa_int > 100  ) {
 
     // Set reward asset. A value of 10000 is equivalent to 1 Telos
-    eosio::asset reward = eosio::asset( reward_amount, symbol(symbol_code("TLOS"),4));
+    eosio::asset reward = eosio::asset( reward_amt, symbol(symbol_code("TLOS"),4));
 
     // Use an inline action to send reward to miner
     // TODO: Permission should be made to somewhere else
     action(
-    permission_level{ get_self(), "active"_n },
-    "eosio.token"_n, "transfer"_n,
-    std::make_tuple( get_self(), miner, reward, miner_memo)
+        permission_level{ get_self(), "active"_n },
+        "eosio.token"_n, "transfer"_n,
+        std::make_tuple( get_self(), miner, reward, string("") )
     ).send();
     
-    _launches.modify(last_itr, get_self(), [&](auto& launch) {
-      launch.reward_sent = true;
+    _launches.modify(launch_itr, get_self(), [&](auto& launch) {
+      launch.level_reached = last_level - 100;
     });
   }
 
   // Add the row to the observation set
   _observations.emplace(get_self(), [&](auto& obs) {
-      obs.launch_number = launch_number;
-      obs.unix_time = unix_time;
-      obs.pressure_hpa = pressure_hpa;
-      obs.temperature_c = temperature_c;
-      obs.relHumidity = relHumidity;
-      obs.dewPoint_c = dewPoint_c;
-      obs.wind_mph = wind_mph;
-      obs.wind_degrees = wind_degrees;
-    });
+    obs.launch_id= launch_id;
+    obs.unix_time = unix_time;
+    obs.pressure_hpa = pressure_hpa;
+    obs.temperature_c = temperature_c;
+    obs.dewPoint_c = dewPoint_c;
+  });
 
 }
 
+ACTION wxlaunches::websetsixhr(name launch_id,
+                               name miner, 
+                               string wx6hrcondition) {
+  // Only web app can run this Action
+  require_auth("weatherapppp"_n);
+
+  launches_table_t _launches(get_self(), get_self().value);  
+  auto launch_itr = _launches.find(launch_id.value);
+
+  // Add the currently stored variables to the launches table
+  if ( launch_itr->miner == miner )
+  {
+    _launches.modify(launch_itr, get_self(), [&](auto& launch) {
+      launch.wx6hrcondition = wx6hrcondition;
+    });
+  }
+
+}   
+
 // Dispatch the actions to the blockchain
-EOSIO_DISPATCH(wxlaunches, (addobs)(websetlaunch)(iotsetlaunch))
+EOSIO_DISPATCH(wxlaunches, (addobs)(setstation)(websetlaunch)(websetsixhr))
